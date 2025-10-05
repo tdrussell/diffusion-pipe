3c8728560f7b7327001a9f85a419885cf6c5cfc3
tdrussell <6509934+tdrussell@users.noreply.github.com>
2025-10-05 18:57:16 -0500

    --reset_optimizer_params flag

--- train.py
+++ train.py
@@ -46,6 +46,7 @@ parser.add_argument('--resume_from_checkpoint', nargs='?', const=True, default=N
                     help='resume training from checkpoint. If no value is provided, resume from the most recent checkpoint. If a folder name is provided, resume from that specific folder.')
 parser.add_argument('--reset_dataloader', action='store_true', help='Start dataloader from scratch when resuming from checkpoint, i.e. only load the optimizer states.')
 parser.add_argument('--reset_optimizer', action='store_true')
+parser.add_argument('--reset_optimizer_params', action='store_true')
 parser.add_argument('--regenerate_cache', action='store_true', help='Force regenerate cache.')
 parser.add_argument('--cache_only', action='store_true', help='Cache model inputs then exit.')
 parser.add_argument('--trust_cache', action='store_true', help='Load from metadata cache files if they exist, without checking if any fingerprints have changed. Can make loading much faster for large datasets.')
@@ -747,12 +748,15 @@ if __name__ == '__main__':
     # make sure to do this before calling model_engine.set_dataloader(), as that method creates an iterator
     # which starts creating dataloader internal state
     if resume_from_checkpoint:
+        param_groups = optimizer.param_groups.copy()
         load_path, client_state = model_engine.load_checkpoint(
             run_dir,
             load_module_strict=False,
-            load_lr_scheduler_states='force_constant_lr' not in config and not args.reset_optimizer,
+            load_lr_scheduler_states='force_constant_lr' not in config and not args.reset_optimizer and not args.reset_optimizer_params,
             load_optimizer_states=not args.reset_optimizer,
         )
+        if args.reset_optimizer_params:
+            optimizer.param_groups = param_groups
         dist.barrier()  # just so the print below doesn't get swamped
         assert load_path is not None
         if args.reset_dataloader:
